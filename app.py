ef7a62733a5ddc6dbb332dd7257acb54dd9b4a5f
tmmarquess <thiago.uchoa18@gmail.com>
2023-01-16 00:56:20 -0400

    adding script to return current ip address

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ from src.modules.controllers.home_controller import *
 from src.modules.controllers.event_controller import *
 from src.modules.controllers.user_controller import *
 from src.resources.app import *
+from get_ip import get_ip
 
 
 if __name__ == "__main__":
-    app.run(host="192.168.0.120", port=5000, debug=False, threaded=True)
+    app.run(host=get_ip(), port=5000, debug=False, threaded=True)
